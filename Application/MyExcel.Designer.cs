473d776c6b43953c2bd1da04e06910a84adeef48
Mikhail Syvachenko <misha.sivachenko@gmail.com>
2022-10-17 13:42:46 +0200

    L1-0000 Refactoring.

--- Application/MyExcel.Designer.cs
+++ Application/MyExcel.Designer.cs
@@ -1,4 +1,5 @@
 ﻿using System.Windows.Forms;
+using System.Drawing;
 
 namespace Excel
 {
@@ -55,20 +56,20 @@ namespace Excel
             // 
             this.panel1.Anchor = AnchorStyles.Top | AnchorStyles.Left;
             this.panel1.Controls.Add(this.dataGridView1);
-            this.panel1.Location = new System.Drawing.Point(1, 47);
+            this.panel1.Location = new Point(1, 47);
             this.panel1.Name = "panel1";
-            this.panel1.Size = new System.Drawing.Size(1136, 595);
+            this.panel1.Size = new Size(1136, 595);
             this.panel1.TabIndex = 1;
             // 
             // dataGridView1
             // 
             this.dataGridView1.ColumnHeadersHeightSizeMode = DataGridViewColumnHeadersHeightSizeMode.AutoSize;
             this.dataGridView1.Dock = DockStyle.Fill;
-            this.dataGridView1.Location = new System.Drawing.Point(0, 0);
+            this.dataGridView1.Location = new Point(0, 0);
             this.dataGridView1.Name = "dataGridView1";
             this.dataGridView1.RowHeadersWidth = 62;
             this.dataGridView1.RowTemplate.Height = 33;
-            this.dataGridView1.Size = new System.Drawing.Size(1136, 595);
+            this.dataGridView1.Size = new Size(1136, 595);
             this.dataGridView1.TabIndex = 0;
             this.dataGridView1.CellClick += new DataGridViewCellEventHandler(this.dataGridView1_CellClick);
             // 
@@ -76,7 +77,7 @@ namespace Excel
             // 
             this.splitContainer1.Cursor = Cursors.VSplit;
             this.splitContainer1.FixedPanel = FixedPanel.Panel1;
-            this.splitContainer1.Location = new System.Drawing.Point(1, 2);
+            this.splitContainer1.Location = new Point(1, 2);
             this.splitContainer1.Name = "splitContainer1";
             // 
             // splitContainer1.Panel1
@@ -94,15 +95,15 @@ namespace Excel
             this.splitContainer1.Panel2.Controls.Add(this.label1);
             this.splitContainer1.Panel2.Controls.Add(this.saveButton);
             this.splitContainer1.Panel2.Controls.Add(this.openButton);
-            this.splitContainer1.Size = new System.Drawing.Size(1136, 39);
+            this.splitContainer1.Size = new Size(1136, 39);
             this.splitContainer1.SplitterDistance = 488;
             this.splitContainer1.TabIndex = 2;
             // 
             // calculateButton
             // 
-            this.calculateButton.Location = new System.Drawing.Point(359, 4);
+            this.calculateButton.Location = new Point(359, 4);
             this.calculateButton.Name = "calculateButton";
-            this.calculateButton.Size = new System.Drawing.Size(126, 34);
+            this.calculateButton.Size = new Size(126, 34);
             this.calculateButton.TabIndex = 1;
             this.calculateButton.Text = "Calculate";
             this.calculateButton.UseVisualStyleBackColor = true;
@@ -110,27 +111,27 @@ namespace Excel
             // 
             // textBox1
             // 
-            this.textBox1.Location = new System.Drawing.Point(3, 5);
+            this.textBox1.Location = new Point(3, 5);
             this.textBox1.Name = "textBox1";
-            this.textBox1.Size = new System.Drawing.Size(350, 31);
+            this.textBox1.Size = new Size(350, 31);
             this.textBox1.TabIndex = 0;
             // 
             // delRowButton
             // 
-            this.delRowButton.Location = new System.Drawing.Point(309, 5);
+            this.delRowButton.Location = new Point(309, 5);
             this.delRowButton.Name = "delRowButton";
-            this.delRowButton.Size = new System.Drawing.Size(30, 32);
+            this.delRowButton.Size = new Size(30, 32);
             this.delRowButton.TabIndex = 7;
             this.delRowButton.Text = "-";
-            this.delRowButton.TextAlign = System.Drawing.ContentAlignment.TopCenter;
+            this.delRowButton.TextAlign = ContentAlignment.TopCenter;
             this.delRowButton.UseVisualStyleBackColor = true;
             this.delRowButton.Click += new System.EventHandler(this.delRowButton_Click);
             // 
             // addRowButton
             // 
-            this.addRowButton.Location = new System.Drawing.Point(273, 5);
+            this.addRowButton.Location = new Point(273, 5);
             this.addRowButton.Name = "addRowButton";
-            this.addRowButton.Size = new System.Drawing.Size(30, 32);
+            this.addRowButton.Size = new Size(30, 32);
             this.addRowButton.TabIndex = 6;
             this.addRowButton.Text = "+";
             this.addRowButton.UseVisualStyleBackColor = true;
@@ -138,29 +139,29 @@ namespace Excel
             // 
             // delColButton
             // 
-            this.delColButton.Location = new System.Drawing.Point(140, 4);
+            this.delColButton.Location = new Point(140, 4);
             this.delColButton.Name = "delColButton";
-            this.delColButton.Size = new System.Drawing.Size(30, 32);
+            this.delColButton.Size = new Size(30, 32);
             this.delColButton.TabIndex = 5;
             this.delColButton.Text = "-";
-            this.delColButton.TextAlign = System.Drawing.ContentAlignment.TopCenter;
+            this.delColButton.TextAlign = ContentAlignment.TopCenter;
             this.delColButton.UseVisualStyleBackColor = true;
             this.delColButton.Click += new System.EventHandler(this.delColButton_Click);
             // 
             // rowLabel
             // 
             this.rowLabel.AutoSize = true;
-            this.rowLabel.Location = new System.Drawing.Point(213, 9);
+            this.rowLabel.Location = new Point(213, 9);
             this.rowLabel.Name = "rowLabel";
-            this.rowLabel.Size = new System.Drawing.Size(54, 25);
+            this.rowLabel.Size = new Size(54, 25);
             this.rowLabel.TabIndex = 4;
             this.rowLabel.Text = "Rows";
             // 
             // addColButton
             // 
-            this.addColButton.Location = new System.Drawing.Point(104, 4);
+            this.addColButton.Location = new Point(104, 4);
             this.addColButton.Name = "addColButton";
-            this.addColButton.Size = new System.Drawing.Size(30, 32);
+            this.addColButton.Size = new Size(30, 32);
             this.addColButton.TabIndex = 3;
             this.addColButton.Text = "+";
             this.addColButton.UseVisualStyleBackColor = true;
@@ -169,17 +170,17 @@ namespace Excel
             // label1
             // 
             this.label1.AutoSize = true;
-            this.label1.Location = new System.Drawing.Point(16, 7);
+            this.label1.Location = new Point(16, 7);
             this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(82, 25);
+            this.label1.Size = new Size(82, 25);
             this.label1.TabIndex = 2;
             this.label1.Text = "Columns";
             // 
             // saveButton
             // 
-            this.saveButton.Location = new System.Drawing.Point(545, 3);
+            this.saveButton.Location = new Point(545, 3);
             this.saveButton.Name = "saveButton";
-            this.saveButton.Size = new System.Drawing.Size(96, 34);
+            this.saveButton.Size = new Size(96, 34);
             this.saveButton.TabIndex = 1;
             this.saveButton.Text = "Save";
             this.saveButton.UseVisualStyleBackColor = true;
@@ -187,9 +188,9 @@ namespace Excel
             // 
             // openButton
             // 
-            this.openButton.Location = new System.Drawing.Point(431, 3);
+            this.openButton.Location = new Point(431, 3);
             this.openButton.Name = "openButton";
-            this.openButton.Size = new System.Drawing.Size(97, 34);
+            this.openButton.Size = new Size(97, 34);
             this.openButton.TabIndex = 0;
             this.openButton.Text = "Open";
             this.openButton.UseVisualStyleBackColor = true;
@@ -197,9 +198,9 @@ namespace Excel
             // 
             // MyExcel
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(10F, 25F);
+            this.AutoScaleDimensions = new SizeF(10F, 25F);
             this.AutoScaleMode = AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(1136, 642);
+            this.ClientSize = new Size(1136, 642);
             this.Controls.Add(this.panel1);
             this.Controls.Add(this.splitContainer1);
             this.Name = "MyExcel";
